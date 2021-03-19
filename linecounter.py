aef8cd3c8a0a7c4313a4d7b4c9bd3a7381b68bef
Anastasiya Filatova <anastafilata@gmail.com>
2021-03-19 20:05:10 +0300

    all right

--- linecounter.py
+++ linecounter.py
@@ -7,18 +7,29 @@ from pprint import pprint
 def text_sort_by_line():
     """ объединяет текст из трех файлов по количеству строк"""
 
+    cooment_text = {}
+
     with open('1.txt', encoding='UTF-8') as t1:
         text1 = t1.readlines()
+        cooment_text[len(text1)] = ['1.txt', text1]
 
     with open('2.txt', encoding='UTF-8') as t2:
         text2 = t2.readlines()
+        cooment_text[len(text2)] = ['2.txt', text2]
 
     with open('3.txt', encoding='UTF-8') as t3:
         text3 = t3.readlines()
+        cooment_text[len(text3)] = ['3.txt', text3]
 
+    with open('union_text.txt', 'a', encoding='UTF-8') as union:
+        list_long = list(cooment_text.keys())
+        list_long.sort()
+        for i in list_long:
+            union.write(f'{cooment_text[i][0]}\n')
+            union.write(f'{str(i)}\n')
+            union.write(f'{"".join(cooment_text[i][1])}\n')
 
-
-    pprint(len(text3))
+    return
 
 
 text_sort_by_line()
